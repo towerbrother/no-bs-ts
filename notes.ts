68619c0c9120010a42a4f384e9147cf0840542a2
Giorgio Torre <giorgio.torre8@gmail.com>
2022-01-21 17:39:28 +0100

    added quote from TS Handbook

--- notes.ts
+++ notes.ts
@@ -12,6 +12,10 @@
 
 /**
  * ? why Typescript?
+ * TS Handbook "The goal of TypeScript is to be a static typechecker for JavaScript
+ * programs - in other words, a tool that runs before your code runs (static)
+ * and ensures that the types of the program are correct (typechecked)."
+ *
  * try to call null/undefined, function with wrong parameters
  * help code faster, what are the right fields
  *
